f359c0e1533dca8d02fa9d7d11b10b4b61317787
GabrielVictorino8266 <gabriel.victorino2004@gmail.com>
2025-05-18 18:42:28 -0300

    fix: corrected imports and sensitive cases

--- backend/api/users/auth_services.py
+++ backend/api/users/auth_services.py
@@ -5,10 +5,16 @@ from bson.objectid import ObjectId
 from jose import jwt, JWTError
 from django.conf import settings
 
-# mongo connection
-users_collection = settings.mongodb['users']
+from api.settings import mongodb  # Se 'api' é o módulo principal
+# Ou:
+import sys
+sys.path.append('/app')  # Adiciona diretório raiz ao path
+from api.settings import mongodb  # Agora deve funcionar
 
-pwd_context = CryptContext(schemes=["bcrypt"], depracated="auto")
+# mongo connection
+users_collection = mongodb['users']
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password, hashed_password):
     """
@@ -28,12 +34,14 @@ def auth_user(cpf: str, password: str):
     """
     Authenticate a user by checking cpf and password.
     """
+    # print(f"Authenticating user with CPF: {cpf}, {password}")
     user = users_collection.find_one({"cpf": cpf})
 
     if not user:
         return None
     
     if not verify_password(password, user.get("password")):
+        print("Password verification failed")
         return None
     
     return user
@@ -51,7 +59,7 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
 
     to_encode.update({"exp": expire})
 
-    enconded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    enconded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.SIMPLE_JWT['ALGORITHM'])
     return enconded_jwt
 
 def create_refresh_token(data: dict, expires_delta: timedelta = None):
@@ -67,7 +75,7 @@ def create_refresh_token(data: dict, expires_delta: timedelta = None):
     
     to_encode.update({"exp": expire, "token_type": "refresh"})
 
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.SIMPLE_JWT['ALGORITHM'])
     return encoded_jwt
 
 def decode_token(token:str):
@@ -75,7 +83,7 @@ def decode_token(token:str):
         Decode jwt token
     """
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithm=settings.SIMPLE_JWT['ALGORITHM'])
         return payload
     except JWTError:
         return None
--- backend/api/users/controller.py
+++ backend/api/users/controller.py
@@ -1,5 +1,126 @@
-from django.contrib.auth.models import User
-from django.db import IntegrityError
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from datetime import timedelta
+from bson.json_util import dumps
+import json
+from .auth_services import auth_user, create_access_token, create_refresh_token, get_user_from_token
+from .authentication import MongoJWTAuthtentication
 
-def autenticate_user(username, email, password, first_name='', last_name=''):
-    pass
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login(request):
+    """
+    Login endpoint for user authentication.
+    """
+    cpf = request.data.get('cpf')
+    password = request.data.get('password')
+
+    if not cpf or not password:
+        return Response(
+            {
+                "detail": "CPF and password are required."
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    user = auth_user(cpf, password)
+    if not user:
+        return Response(
+            {
+                "detail": "Invalid credentials."
+            },
+            status=status.HTTP_401_UNAUTHORIZED
+        )
+    
+    user_dict = json.loads(dumps(user))
+
+    #create tokens
+    access_token_expires = timedelta(hours=1)
+    refresh_token_expires = timedelta(days=1)
+
+    access_token = create_access_token(
+        data={
+            "user_id": user_dict.get('_id'),
+            "cpf": user_dict.get('cpf'),
+            "type": user_dict.get('type'),
+        },
+        expires_delta=access_token_expires
+    )
+
+    refresh_token = create_refresh_token(
+        data={
+            "user_id": user_dict.get('_id'),
+            "cpf": user_dict.get('cpf'),
+            "type": user_dict.get('type'),
+        },
+        expires_delta=refresh_token_expires
+    )
+
+
+    return Response({
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "user": {
+            "id": str(user_dict.get('_id')),
+            "cpf": user_dict.get('cpf'),
+            "type": user_dict.get('type'),
+        }
+    })
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def refresh_token(request):
+    """
+    Refresh token endpoint.
+    """
+    refresh = request.data.get('refresh_token')
+    if not refresh:
+        return Response(
+            {"detail": "Refresh token is required."},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    user = get_user_from_token(refresh)
+    if not user:
+        return Response(
+            {"detail": "Invalid refresh token."},
+            status=status.HTTP_401_UNAUTHORIZED
+        )
+    
+    user_dict = json.loads(dumps(user))
+    access_token_expires = timedelta(hours=1)
+    refresh_token_expires = timedelta(days=1)
+
+    access_token = create_access_token(
+        data={
+            "user_id": user_dict.get('_id'),
+            "cpf": user_dict.get('cpf'),
+            "type": user_dict.get('type'),
+        }    
+    )
+
+    return Response({
+        "access_token": access_token,
+        "token_type": "bearer"
+    })
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+@authentication_classes([MongoJWTAuthtentication])
+def get_user_profile(request):
+    """
+    Get user endpoint and returns user profile which is logged in.
+    """
+    user = request.user
+    user_dict = json.loads(dumps(user))
+
+    return Response({
+        "user": {
+            "id": str(user_dict.get('_id')),
+            "cpf": user_dict.get('cpf'),
+            "type": user_dict.get('type'),
+            "message": "User profile retrieved successfully."
+        }
+    })
