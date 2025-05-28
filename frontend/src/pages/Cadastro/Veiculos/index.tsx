80c77732f6f6671390303c502a30f9e6f24d3f0d
Raphael <raphaelreisrodrigues@hotmail.com>
2025-05-28 00:04:33 -0300

    Save

--- frontend/src/pages/Cadastro/Veiculos/index.tsx
+++ frontend/src/pages/Cadastro/Veiculos/index.tsx
@@ -1,11 +1,24 @@
 import { Sidebar } from "../../../components/Sidebar" 
+import { Container, Main, Header, Form, BoxForm } from "./styles"
 
 export function CadastroVeiculo(){
     return(
 
-        <div>
-            <Sidebar />    
-        </div>
+        <Container>
+            <Sidebar />  
+            <Main>
+                <Header>
+                    <h1>Cadastro de viagem</h1>
+                </Header>
+                
+                <BoxForm>
+                    <Form>
+                        
+                    </Form>
+                </BoxForm>
+
+            </Main>  
+        </Container>
         
     )
 }
---
+++ frontend/src/pages/Cadastro/Veiculos/styles.ts
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled'
+
+export const Container = styled.div`
+    display: flex;
+`
+
+export const Main = styled.div`
+    width: 100%;
+    background-color: #DDE5F0FA;
+`
+
+export const Header = styled.div`
+    background-color: #ffffff;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    padding-left: 20px;
+
+    h1{
+        font-size: 40px;
+        color: #071425;
+        font-weight: 700;
+    }
+`
+
+export const BoxForm = styled.div`
+    width: 100%;
+    height: 92.7%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Form = styled.form`
+    width: 1008px;
+    height: 698px;
+    border-radius: 38px;
+    background-color: #071425;
+`
