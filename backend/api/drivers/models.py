a80e34843050be20f760153d026fe9dd79e7de63
GabrielVictorino8266 <gabriel.victorino2004@gmail.com>
2025-06-05 21:30:34 -0300

    feat: add drivers model and update dependencies

--- backend/api/drivers/models.py
+++ backend/api/drivers/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from mongoengine import Document, StringField, IntField, ListField, ReferenceField, BooleanField
+import datetime
 
-# Create your models here.
+class Driver(Document):
+    password = StringField(required=True)
+    cpf = StringField(required=True, unique=True)
+    email = StringField(required=True)
+    name =StringField(required=True)
+    birthDate = datetime.date
+    phone = StringField(required=True)
+    licenseType = StringField(required=True)
+    licenseNumber = StringField(required=True)
+    performance = IntField(default=0)
+    incidents = ListField(StringField(), default=[])
+    isActive = BooleanField(default=True)
+    type = StringField(default='Motorista')
