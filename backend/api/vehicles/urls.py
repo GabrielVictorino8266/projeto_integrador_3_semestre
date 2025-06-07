69e0e25fcf67657a7924ad914bb3efab883a4dbb
bruno <bruno.santos382@fatec.sp.gov.br>
2025-06-07 11:32:35 -0300

    feat(vehicle): add endpoint to retrieve list status

--- backend/api/vehicles/urls.py
+++ backend/api/vehicles/urls.py
@@ -5,7 +5,8 @@ from .swagger.vehicle_swagger import (
     update_vehicle_swagger,
     delete_vehicle_swagger,
     list_vehicles_swagger,
-    get_vehicle_swagger
+    get_vehicle_swagger,
+    list_vehicle_status_swagger
 )
 
 app_name = 'vehicles'
@@ -13,6 +14,7 @@ urlpatterns = [
     path('create/', create_vehicle_swagger(views.create_vehicle), name='create_vehicle'),
     path('update/<str:id>/', update_vehicle_swagger(views.update_vehicle), name='update_vehicle'),
     path('delete/<str:id>/', delete_vehicle_swagger(views.delete_vehicle), name='delete_vehicle'),
+    path('status/', list_vehicle_status_swagger(views.list_vehicle_status), name='list_vehicle_status'),
     path('<str:id>/', get_vehicle_swagger(views.get_vehicle), name='get_vehicle'),
     path('', list_vehicles_swagger(views.list_vehicles), name='list_vehicles'),
 ]
