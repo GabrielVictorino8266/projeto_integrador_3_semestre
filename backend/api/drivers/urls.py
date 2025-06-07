19e67b64d7f5092e2bf34da7c233f73c4295996e
GabrielVictorino8266 <gabriel.victorino2004@gmail.com>
2025-06-07 07:30:50 -0300

    feat: update URL paths for drivers app

--- backend/api/drivers/urls.py
+++ backend/api/drivers/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from . import views
 
-router = DefaultRouter()
-router.register('drivers', views.DriverViewSet, basename='driver')
-
 urlpatterns = [
-    path('', include(router.urls)),
-    path('drivers/', views.DashbiardView.as_view(), name='dashboard'),
+    path('list', views.list_drivers, name='list_drivers'),
 ]
