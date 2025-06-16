95c3840fc65745dd46239fecc2eb6d2062825c06
Raphael <raphaelreisrodrigues@hotmail.com>
2025-06-15 23:32:39 -0300

    fix: Implemented license plate mask for vehicle Dashboard

--- frontend/src/components/Dashboard/Vehicles/VehicleTableItems/index.tsx
+++ frontend/src/components/Dashboard/Vehicles/VehicleTableItems/index.tsx
@@ -2,23 +2,29 @@
 import type { IVehicle } from "@interfaces/vehicles.interface";
 import { VehicleListItem } from "./Table";
 import { StyledDriverList } from "./styles";
+import { MaskLicensePlate } from "@utils/Mask/MaskLicensePlate";
 
 interface Props {
-  data: IVehicle[];
+    data: IVehicle[];
 }
 
-export const VehicleList = ({ data }: Props) => (
-  <StyledDriverList>
-    {data.map((v) => (
-      <VehicleListItem
-        key={v.id}
-        id={v.id}
-        licensePlate={v.licensePlate}
-        vehicleType={v.vehicleType}
-        manufacturingYear={v.manufacturingYear}
-        brand={v.brand}
-        status={v.status}
-      />
-    ))}
-  </StyledDriverList>
-);
+export const VehicleList = ({ data }: Props) => {
+
+    const plateMask = new MaskLicensePlate()
+
+    return (
+        <StyledDriverList>
+            {data.map((v) => (
+                <VehicleListItem
+                    key={v.id}
+                    id={v.id}
+                    licensePlate={plateMask.mask(v.licensePlate)}
+                    vehicleType={v.vehicleType}
+                    manufacturingYear={v.manufacturingYear}
+                    brand={v.brand}
+                    status={v.status}
+                />
+            ))}
+        </StyledDriverList>
+    );
+};
