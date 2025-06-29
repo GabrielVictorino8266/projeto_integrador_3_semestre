a4da52ac299dea53b63d6f6d195c8cf0bdbd2f3c
Raphael <raphaelreisrodrigues@hotmail.com>
2025-06-29 20:41:51 -0300

    fix: updated styles input

--- frontend/src/components/InputForm/styles.ts
+++ frontend/src/components/InputForm/styles.ts
@@ -2,44 +2,48 @@ import styled from "@emotion/styled";
 import type { FieldError } from "react-hook-form";
 
 interface StyledInputContainerProps {
-  error?: FieldError;
+    error?: FieldError;
 }
 
 const StyledInput = styled.fieldset<StyledInputContainerProps>`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-
-  label {
-    font-weight: var(--font-weight-700);
-  }
-
-  label,
-  input {
-    font-family: var(--font-inter);
-    font-size: var(--font-size-20);
-  }
-
-  input {
-    border-radius: var(--border-radius-32);
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.25);
     width: 100%;
-    height: 52px; // mudar para rem.
-    padding: 8px 20px;
-    background-color: ${(props) =>
-      props.error ? "var(--color-error-2)" : "var(--color-input-background)"};
-  }
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
 
-  .inputErrorMessage {
-    color: var(--color-error);
-    font-weight: 500;
-    font-size: var(--font-size-12);
-  }
+    label {
+        font-weight: var(--font-weight-700);
+    }
 
-  p {
-    height: 12px;
-  }
+    label,
+    input {
+        font-family: var(--font-inter);
+        font-size: var(--font-size-20);
+    }
+
+    input {
+        border-radius: var(--border-radius-32);
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.25);
+        width: 100%;
+        border: none;
+        height: 56px;
+        padding: 8px 20px;
+        background-size: 24px 24px;
+        font-size: 20px;
+        font-weight: 700;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.25);
+        background-color: ${(props) => (props.error ? "var(--color-error-2)" : "var(--color-input-background)")};
+    }
+
+    .inputErrorMessage {
+        color: var(--color-error);
+        font-weight: 500;
+        font-size: var(--font-size-12);
+    }
+
+    p {
+        height: 12px;
+    }
 `;
 
 export { StyledInput };
--- frontend/src/utils/Mask/ApplyMask.ts
+++ frontend/src/utils/Mask/ApplyMask.ts
@@ -1,17 +1,14 @@
 import { FactoryMask } from "./FactoryMask";
 import type { MaskType } from "./TypeMask";
 
-export function ApplyMask(
-  event: React.ChangeEvent<HTMLInputElement>,
-  mask?: MaskType
-): string {
-  const valueChange = event.target.value;
+export function ApplyMask(event: React.ChangeEvent<HTMLInputElement>, mask?: MaskType): string {
+    const valueChange = event.target.value;
 
-  if (mask) {
-    const instancia = FactoryMask(mask);
-    const formatted = instancia.mask(valueChange);
-    return formatted;
-  }
+    if (mask) {
+        const instancia = FactoryMask(mask);
+        const formatted = instancia.mask(valueChange);
+        return formatted;
+    }
 
-  return valueChange;
+    return valueChange;
 }
