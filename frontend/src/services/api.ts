f928fb267635db3b0489fd0cb10b2995654aee87
Juliano Santos <julianosantosdev@outlook.com>
2025-07-12 12:35:41 -0300

    add: render api endpoint in frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// export const api = axios.create({
+//   baseURL: 'http://localhost:8000/api'
+// });
+
 export const api = axios.create({
-  baseURL: 'http://localhost:8000/api'
+  baseURL: 'https://projeto-integrador-3-semestre.onrender.com'
 });
 
 api.interceptors.request.use((config) => {
